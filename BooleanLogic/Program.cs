0699059b56b76e1ee11409800c07627e59cd35fd
Michael Walker <walkerrepair255@yahoo.com>
2021-12-07 16:05:41 -0500

    bool logic

---
+++ BooleanLogic/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ^ returns true if both are not equal. 
+            Console.WriteLine(true ^ true);    // output: False
+            Console.WriteLine(true ^ false);   // output: True
+            Console.WriteLine(false ^ true);   // output: True
+            Console.WriteLine(false ^ false);  // output: False
+        }
+    }
+}
